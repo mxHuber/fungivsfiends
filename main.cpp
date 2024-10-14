73cfd0c3736c50e14be20b8ad8cd05851f29707b
mxHuber <huber.maximilian.leo@gmail.com>
2024-10-14 18:28:19 +0200

    basic running version

--- main.cpp
+++ main.cpp
@@ -1,1 +1,11 @@
- 
+#include <QuadGenerator.hpp>
+#include <Renderer.hpp>
+
+int main() {
+  mlh::Renderer App = mlh::Renderer();
+
+  App.setShader("../mlh-renderer/resources/Shaders/Vertex.shader",
+                "../mlh-renderer/resources/Shaders/Fragment.shader");
+  App.addQuad(mlh::QuadGenerator::getQuad());
+  App.runLoop();
+}
